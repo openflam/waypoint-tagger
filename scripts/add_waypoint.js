ff9640e24c2417739be219778a7dbbb85c50967e
Sagar Bharadwaj <skalasib@andrew.cmu.edu>
2024-06-02 14:33:02 -0400

    Added balloon asset for waypoint

--- scripts/add_waypoint.js
+++ scripts/add_waypoint.js
@@ -1,6 +1,7 @@
 function addWaypoint() {
     var waypointEntity = document.createElement("a-entity");
     waypointEntity.setAttribute("way_point", '');
+    waypointEntity.setAttribute("gltf-model", "#waypoint_model");
 
     const ascene = document.querySelector("a-scene");
     ascene.appendChild(waypointEntity);
--- scripts/way_point_component.js
+++ scripts/way_point_component.js
@@ -3,21 +3,5 @@ AFRAME.registerComponent('way_point',{
         ID: {type: 'string'},
         name: {type: 'string'},
         description: {type: 'string'},
-    },
-    init: function(){
-        var data = this.data;
-        var el = this.el;
-        this.geometry = new THREE.SphereGeometry(0.5,32,16);
-        this.material = new THREE.MeshBasicMaterial({color: 0xb5b5b5});
-        this.mesh = new THREE.Mesh(this.geometry,this.material);
-        el.setObject3D('mesh',this.mesh);
-    },
-    update: function(oldData){
-        var data = this.data;
-        var el = this.el;
-        if (Object.keys(oldData).length === 0) { return; }
-    },
-    remove: function(){
-        this.el.removeObject3D('mesh');
     }
-})
+});
