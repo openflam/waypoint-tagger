338dbacc6c49a0b3f6611d8013243538b99617bc
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2024-06-20 17:11:51 -0400

    Added remove lines logic

--- scripts/components/waypoint.js
+++ scripts/components/waypoint.js
@@ -43,20 +43,30 @@ AFRAME.registerComponent('way_point', {
 
         // Update neighbors
         if (data.neighbors !== oldData.neighbors) {
-            var neighbors = data.neighbors.split(",");
-            for (neighbor of neighbors) {
-                // Update the neighbor's neighbor list
-                try {
-                    var neighborEntity = el.sceneEl.querySelector("#" + neighbor);
-                    var neighborComponent = neighborEntity.components.way_point;
-                }
-                catch (error) {
-                    // Inspector calls the setAttribute for every character entered. 
-                    // The catch block is used to prevent the error message of non-existing element
-                    // from being displayed.
-                    continue;
-                }
+            var neighbors_list = this.getValidNeighbors(data.neighbors);
+            var oldNeighbors_list = this.getValidNeighbors(oldData.neighbors);
 
+            console.log("New neighbors and old neighbors:");
+            console.log(neighbors_list);
+            console.log(oldNeighbors_list);
+
+            // Get the neighbors that are removed and added
+            var removedNeighbors = oldNeighbors_list.filter(
+                (id) => (!neighbors_list.includes(id))
+            );
+            var addedNeighbors = neighbors_list.filter(
+                (id) => (!oldNeighbors_list.includes(id))
+            );
+
+            console.log("Removed neighbors and added neighbors:");
+            console.log(removedNeighbors);
+            console.log(addedNeighbors);
+
+            // Add new neighbor
+            for (neighbor of addedNeighbors) {
+                // Update the neighbor's neighbor list
+                var neighborEntity = el.sceneEl.querySelector("#" + neighbor);
+                var neighborComponent = neighborEntity.components.way_point;
                 var neighborNeighbors = neighborComponent.data.neighbors.split(",");
                 // If the neighbor is not in the neighbor list, add it
                 if (neighborNeighbors.length === 1 && neighborNeighbors[0] === "") {
@@ -70,7 +80,35 @@ AFRAME.registerComponent('way_point', {
                 // Add a line between this waypoint and the neighbor
                 this.addLineToNeighbor(neighbor);
             }
+
+            // Remove the removed neighbor
+            for (neighbor of removedNeighbors) {
+                // Update the neighbor's neighbor list
+                var neighborEntity = el.sceneEl.querySelector("#" + neighbor);
+                var neighborComponent = neighborEntity.components.way_point
+                var neighborNeighbors = neighborComponent.data.neighbors.split(",");
+
+                // Remove the this item from the neighbor list
+                neighborNeighbors = neighborNeighbors.filter(
+                    (id) => (id != data.ID)
+                );
+
+                // Update the neighbor's neighbor list
+                neighborComponent.data.neighbors = neighborNeighbors.join(",");
+
+                // Remove the line between this waypoint and the neighbor
+                this.removeLineToNeighbor(neighbor);
+            }
         }
+    },
+
+    getValidNeighbors: function (neighbors) {
+        neighbors = neighbors.split(",");
+        neighbors = neighbors.map((id) => id.trim());
+        valid_neighbors = neighbors.filter(
+            (id) => ((id !== "") && (this.el.sceneEl.querySelector("#" + id) !== null))
+        );
+        return valid_neighbors;
     },
 
     addLineToNeighbor: function (neighborId) {
@@ -96,5 +134,24 @@ AFRAME.registerComponent('way_point', {
         this.el.sceneEl.appendChild(line);
 
         this.neighborLines.push(line);
+    },
+
+    removeLineToNeighbor: function (neighborId) {
+        var thisId = this.el.getAttribute("id");
+
+        // Assign start and end as alphabetically smaller and larger
+        startId = thisId < neighborId ? thisId : neighborId;
+        endId = thisId < neighborId ? neighborId : thisId;
+
+        // Check if the line already exists
+        lineId = startId + "-" + endId;
+        var line = this.el.sceneEl.querySelector("#" + lineId);
+        if (line) {
+            this.el.sceneEl.removeChild(line);
+        }
+
+        this.neighborLines = this.neighborLines.filter(
+            (line) => (line.getAttribute("id") != lineId)
+        );
     }
 });
