c43070271d069902087f83b7d72a2f229b12df4d
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2025-03-07 16:43:33 -0500

    Added add map from mesh functionality

--- scripts/add_map.js
+++ scripts/add_map.js
@@ -1,15 +1,46 @@
-function loadMapFromFile(fileSelectEvent) {
+function loadMapMeshFromFile(fileSelectEvent) {
     const reader = new FileReader();
 
     reader.addEventListener("load", function (event) {
         mapContent = event.target.result;
-        addMapToScene(mapContent);
+        addMapMeshToScene(mapContent);
     });
 
     reader.readAsArrayBuffer(fileSelectEvent.target.files[0]);
 }
 
-function addMapToScene(mapContent) {
+function addMapMeshToScene(mapContent) {
+    let gltfLoader = new THREE.GLTFLoader();
+    let mapContentBlob = new Blob([mapContent], { type: 'application/octet-stream' });
+    let fileURL = URL.createObjectURL(mapContentBlob);
+
+    gltfLoader.load(fileURL, function (gltf) {
+        let model = gltf.scene;
+
+        let ascene = document.querySelector("a-scene");
+        let mapEntity = document.createElement("a-entity");
+        mapEntity.setObject3D('mesh', model);
+
+        // The raycaster used by inspector will ignore this entity
+        mapEntity.setAttribute("data-aframe-inspector", "");
+
+        ascene.appendChild(mapEntity);
+    }
+    );
+}
+
+function loadMapPCDFromFile(fileSelectEvent) {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function (event) {
+        mapContent = event.target.result;
+        addMapPCDToScene(mapContent);
+    });
+
+    reader.readAsArrayBuffer(fileSelectEvent.target.files[0]);
+}
+
+function addMapPCDToScene(mapContent) {
     pcdLoader = new PCDLoader();
     points = pcdLoader.parse(mapContent, "map.url");
     points.material.size = 0.1;
--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,12 @@ window.onload = function () {
     ascene.addEventListener('loaded', function () {
         ascene.components.inspector.openInspector(true);
 
-        waitForElm("#add-map-input").then((inputElement) => {
-            inputElement.addEventListener("change", loadMapFromFile);
+        waitForElm("#add-map-mesh").then((inputElement) => {
+            inputElement.addEventListener("change", loadMapMeshFromFile);
+        });
+
+        waitForElm("#add-map-pcd").then((inputElement) => {
+            inputElement.addEventListener("change", loadMapPCDFromFile);
         });
 
         waitForElm("#load-waypoints-input").then((inputElement) => {
